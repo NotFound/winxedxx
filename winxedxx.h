e2443bba3ff95dbe478b51f8a6640449124a283f
NotFound <julian.notfound@gmail.com>
2011-02-09 23:18:31 +0100

    var statement, assign int to var

--- winxedxx.h
+++ winxedxx.h
@@ -21,12 +21,31 @@ public:
     int get_integer() { return 0; };
 };
 
+class WinxedxxInteger : public WinxedxxObject
+{
+public:
+    WinxedxxInteger(int value)
+    {
+        i = value;
+    }
+    int get_integer() { return i; };
+private:
+    int i;
+};
+
+//*************************************************************
+
 class WinxedxxObjectPtr
 {
 public:
     WinxedxxObjectPtr() : object(new WinxedxxNull())
     { }
     operator int() const { return object->get_integer(); };
+    WinxedxxObjectPtr & operator = (int i)
+    {
+        object = new WinxedxxInteger(i);
+        return *this;
+    }
 private:
     WinxedxxObject *object;
 };
