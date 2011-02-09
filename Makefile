db56a809b286b11ec8538378c248b609cc510c5e
NotFound <julian.notfound@gmail.com>
2011-02-09 22:22:02 +0100

    first commit, just a few features

---
+++ Makefile
@@ -0,0 +1,12 @@
+all: simple
+	./simple
+
+simple: simple.cxx
+	g++ -Wall -o simple simple.cxx
+
+simple.cxx: simple.winxed winxedxx.winxed
+	winxed -L /home/julian/winxed winxedxx.winxed simple.winxed > simple.cxx
+
+clean:
+	rm -f simple
+	rm -r simple.cxx
---
+++ winxedxx.h
@@ -0,0 +1,42 @@
+#ifndef INCLUDE_WINXEDXX_H
+#define INCLUDE_WINXEDXX_H
+
+// winxedxx.h
+// Revision 9-feb-2011
+
+#include <iostream>
+#include <string>
+
+class WinxedxxObject {
+public:
+    virtual int get_integer()
+    {
+        throw "get_integer not implemented";
+    }
+};
+
+class WinxedxxNull : public WinxedxxObject
+{
+public:
+    int get_integer() { return 0; };
+};
+
+class WinxedxxObjectPtr
+{
+public:
+    WinxedxxObjectPtr() : object(new WinxedxxNull())
+    { }
+    operator int() const { return object->get_integer(); };
+private:
+    WinxedxxObject *object;
+};
+
+static WinxedxxObjectPtr winxedxxnull = WinxedxxObjectPtr();
+
+int wxx_print(int i) { std::cout << i; return 0; }
+int wxx_print(const char *s) { std::cout << s; return 0; }
+int wxx_print(const std::string &s) { std::cout << s; return 0; }
+
+#endif
+
+// End of winxedxx.h
